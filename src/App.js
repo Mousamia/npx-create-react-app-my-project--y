0e68cab5b73a6b37cc0774e7992d3d989c8a0e9f
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-03-22 23:52:20 +0600

    react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
+/* eslint-disable no-unused-vars */
 import logo from './logo.svg';
 import './App.css';
+const number = 4444;
+const person = {name: "sharmin", job: "jalim", production: "joha"}
+const styleHere = {
+  backgroundColor: "red",
+}
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='chehara'>
+        {/* <p>Name: {34577 + number}</p>
+        <p>{person.name}</p>
+        <p style={styleHere}>vallagena kisu</p>
+        <p>ghum ase onek</p>
+        <p style={{backgroundColor: "blue"}}>Sharmin jemon temon jibon e pak</p>
+        <h1>I dont feel good right now</h1> */}
+
+        <Matha> </Matha>
+
+      </div>
     </div>
   );
 }
+
+function Matha(){
+  return (
+   <div> <h2>Only garbage is real</h2>
+   <h1>Garbage felun matha clear krun</h1></div>
+  )
+}
+
+
+
+
 
 export default App;
